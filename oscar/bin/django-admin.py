b901b30d22c79d1729ae8899f31e614b3aaba29b
root <root@ip-172-31-38-136.us-west-2.compute.internal>
2017-05-25 04:51:36 +0000

    First Commit

---
+++ oscar/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/var/www/oscar/oscar/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'Django==1.11.1','django-admin.py'
+__requires__ = 'Django==1.11.1'
+__import__('pkg_resources').run_script('Django==1.11.1', 'django-admin.py')
